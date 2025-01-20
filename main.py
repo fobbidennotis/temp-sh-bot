c42f7b5ec09e7fe484f2360d78ccd32a07630e06
NotisFobbidden <fobbidennotis@gmail.com>
2025-01-20 22:50:47 +0100

    Add PM handling and now uploads are done with a proper extension

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-from aiogram import Dispatcher, Bot
-from aiogram.types import File, InlineQuery, Message
+from aiogram import F, Dispatcher, Bot
+from aiogram.types import ContentType, File, InlineQuery, Message
 from aiogram.filters import Command
 from aiogram.exceptions import TelegramBadRequest
 import requests
@@ -22,11 +22,41 @@ async def file_check(msg: Message):
     return None
 
 
+async def download_and_upload_file(msg) -> str:
+    file_id = await file_check(msg)
+
+    if file_id == None:
+        return "Ensure your message holds a file"
+
+    try:
+        file: File = await bot.get_file(file_id)
+    except TelegramBadRequest:
+        return "Sadly, this bot can only upload media up to 20MB"
+
+    bio = await bot.download(file)
+
+    file_extension = f".{file.file_path.split(".")[-1]}"
+
+    res = requests.post(
+        "https://temp.sh/upload", files={"file": (f"upload{file_extension}", bio)}
+    )
+
+    return res.text
+
+
 @dp.message(Command(commands=["start", "help"]))
 async def start_help_command(msg: Message) -> None:
     await msg.reply(
         "This bot is made as a telegram interface for temp.sh . Convenient for use in groups. To upload a file and retrieve a temporary URL, reply to a message that holds your desired file with the /wrap command. Source code: https://github.com/fobbidennotis/temp-sh-bot"
     )
+
+
+@dp.message(F.content_type == ContentType.VIDEO)
+@dp.message(F.content_type == ContentType.PHOTO)
+@dp.message(F.content_type == ContentType.DOCUMENT)
+@dp.message(F.content_type == ContentType.AUDIO)
+async def handle_sent_file(msg: Message) -> None:
+    await msg.reply(await download_and_upload_file(msg))
 
 
 @dp.message(Command("wrap"))
@@ -35,23 +65,7 @@ async def wrap_command(msg: Message) -> None:
         await msg.reply("You have to reply to a message")
         return
 
-    file_id = await file_check(msg.reply_to_message)
-
-    if file_id == None:
-        await msg.reply("Ensure your message holds a file")
-        return
-
-    try:
-        file: File = await bot.get_file(file_id)
-    except TelegramBadRequest:
-        await msg.reply("Sadly, this bot can only upload files up to 20MB in size")
-        return
-
-    bio = await bot.download(file)
-
-    res = requests.post("https://temp.sh/upload", files={"file": bio})
-
-    await msg.reply(res.text)
+    await msg.reply(await download_and_upload_file(msg.reply_to_message))
 
 
 async def main() -> None:
